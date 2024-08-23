a8c8abea0d3efe2fb44c977d70c2f78d487688e6
process404 <88147999+process404@users.noreply.github.com>
2024-08-23 20:52:14 +0100

    init

---
+++ src/routes/logs/+page.svelte
@@ -0,0 +1,10 @@
+<Nav ver="back" class="w-full"/>
+<div class="h-screen">
+
+</div>
+<Footer/>
+
+<script>
+    import Nav from '../../lib/components/Nav.svelte';
+    import Footer from '../../lib/components/Footer.svelte';
+</script>
