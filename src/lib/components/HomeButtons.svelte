503cec288624b5b762968068507c0882fab211f0
process404 <88147999+process404@users.noreply.github.com>
2024-08-23 21:49:14 +0100

    settings

--- src/lib/components/HomeButtons.svelte
+++ src/lib/components/HomeButtons.svelte
@@ -1,13 +1,21 @@
-<button on:click={() => switchPage()} class="clip w-full h-1/4 relative border-[0px] hover:border-[1px] border-neutral-700 hover:border-white duration-100 group overflow-hidden">
+<button on:click={() => switchPage()} class="clip w-full h-full relative border-[0px] hover:border-[1px] border-neutral-700 hover:border-white duration-100 group overflow-hidden">
     <div class="absolute w-full h-full top-0 left-0 flex items-end justify-end flex-col">
-        <div class="flex h-full items-center mt-2">
-            <h2 class="sm:right-10 right-4 rounded-sm text-white bg-black bg-opacity-50 text-3xl z-20 p-2 pl-8 pr-4 flex items-center top-16 clip mr-4">{buttonName} <span class="ml-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short w-12 h-12 pt-1" viewBox="0 0 16 16">
+        <div class="flex h-full items-center">
+            <h2 class="sm:right-10 right-4 rounded-md text-white bg-black bg-opacity-60 border-[1px] border-neutral-700 text-3xl z-20 p-2 pl-8 pr-4 flex items-center top-16 clip mr-4">{buttonName} <span class="ml-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short w-12 h-12 pt-1" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
             </svg></span></h2>
         </div>
-        <a title="{imgTitle}" href="{imgHref}" class="bg-black bottom-0  mr-auto z-30 text-white bg-opacity-40 text-[0.5rem] p-1 pl-4 pr-2 rounded-sm w-full sm:w-auto">{imgTitle2}</a>
+        {#if imgTitle}
+        <a title="{imgTitle}" href="{imgHref}" class="bg-black bottom-0  mr-auto z-30 text-white bg-opacity-40 text-[0.55rem] p-1 pl-2 pr-2 rounded-sm w-full sm:w-auto">{imgTitle2}</a>
+        {/if}
     </div>
+    {#if imgSrc}
     <img class="absolute top-0 w-full h-full z-10 object-cover blur-sm opacity-100 duration-100 group-hover:opacity-70" alt="FFS RABe 522 205 Basel SBB 180611" src="{imgSrc}">
+    {/if}
+    {#if !imgSrc}
+        <div class="absolute top-0 w-full h-full z-10 bg-neutral-700 bg-opacity-60"></div>
+    }
+    {/if}
 </button>
 
 <script>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,21 @@
 
 <nav class="bg-black w-full min-h-16 p-2 flex mb-4 pl-4 pr-4 items-center justify-center">
     {#if ver == 'main'}
-    <div class="max-w-[1000px] flex justify-between w-full">
-        <h2 class="text-white text-2xl font-semibold">Trip Planner</h2>
-        <div class="flex h-full mt-auto">
-            <span style="clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);" class="w-6 h-2 bg-red-600 block"></span>
-            <span style="clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);" class="w-6 h-2 bg-blue-600 block"></span>
-            <span style="clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);" class="w-6 h-2 bg-neutral-400 block"></span>
+    <div class="max-w-[1000px] flex items-center justify-between w-full gap-4">
+        <div class="flex gap-4 items-center">
+            <h2 class="text-white text-2xl font-semibold">Trip Planner</h2>
+            <div class="flex h-full">
+                <span style="clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);" class="w-6 h-2 bg-red-600 block"></span>
+                <span style="clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);" class="w-6 h-2 bg-blue-600 block"></span>
+                <span style="clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);" class="w-6 h-2 bg-neutral-400 block"></span>
+            </div>
+        </div>
+        <div>
+            <button class="flex items-center" on:click={() => {window.location.href = 'settings'}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill fill-white w-6 h-6 opacity-30 hover:opacity-100 duration-100" viewBox="0 0 16 16">
+                    <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                </svg>
+            </button>
         </div>
     </div>
     {/if}
