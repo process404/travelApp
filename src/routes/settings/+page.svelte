4ae90a4f19e5869aa83f353a755b9e89e9bad49b
process404 <88147999+process404@users.noreply.github.com>
2024-09-02 20:05:04 +0100

    header fix

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center h-screen justify-start">
         <div class="h-full max-w-[1000px] w-full flex flex-col">
             <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-4 sm:mr-8 h-full sm:pt-6 sm:pb-6 pl-2 pr-2 pb-2">
-                <h2 class="text-white text-2xl font-semibold sm:mt-1 m">Settings</h2>
+                <h2 class="text-white text-2xl font-semibold sm:mt-1 mt-4 mb-4">Settings</h2>
             </div>
         </div>
     </div>
