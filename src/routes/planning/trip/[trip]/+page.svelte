a7819ac2575c20577347d01b0b1b1ecea2969442
process404 <88147999+process404@users.noreply.github.com>
2024-09-02 19:48:59 +0100

    reduced width on days

--- src/routes/planning/trip/[trip]/+page.svelte
+++ src/routes/planning/trip/[trip]/+page.svelte
@@ -2,7 +2,7 @@
     <Nav ver="back"/>
         <div class="flex flex-col items-center h-full justify-start overflow-y-scroll customScrollbar overflow-x-hidden">
             <div class="max-w-[1000px] w-full flex flex-col h-full">
-                <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-2 sm:mr-8 sm:pt-4 sm:pb-6 pl-4 pr-4 pb-4 overflow-y-scroll customScrollbar h-full">
+                <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-2 sm:mr-8 sm:pt-4 sm:pb-6 pl-4 pr-3 pb-4 overflow-y-scroll customScrollbar h-full">
                     {#if tripName == ''}
                         <span class="loader"></span>
                     {/if}
@@ -10,12 +10,12 @@
                     {#if thisTrip == null}
                         <span class="loader"></span>
                     {:else}
-                        <div class="w-full">
+                        <div class="w-full pl-1 flex-col justify-center items-center  max-w-[600px]">
                             {#each thisTrip.days as day}
                                 <div class="border-[1px] border-neutral-700 rounded-md w-full p-2 h-auto min-h-[100px] first:mt-0 mt-4">
                                     <div class="flex justify-between w-full items-center">
                                         <h2 class="text-white italic">Day {day.day}</h2>
-                                        <button class="fadeButton blue2 p-1 text-sm">Add Journey</button>
+                                        <button class="fadeButton blue p-1 text-sm">Add Journey</button>
                                     </div>
                                     <hr class="mt-2 border-neutral-700 mb-2">
                                 </div>
