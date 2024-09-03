dd573830e2bdbaab80304180643ef68a1920419d
process404 <88147999+process404@users.noreply.github.com>
2024-09-03 01:49:48 +0100

    added home button touches

--- src/lib/components/HomeButtons.svelte
+++ src/lib/components/HomeButtons.svelte
@@ -1,7 +1,7 @@
 <button on:click={() => switchPage()} class="clip w-full h-full relative border-[0px] hover:border-[1px] border-neutral-700 hover:border-white duration-100 group overflow-hidden">
     <div class="absolute w-full h-full top-0 left-0 flex items-end justify-end flex-col">
-        <div class="flex h-full items-center w-full sm:w-auto justify-center sm:justify-normal">
-            <h2 class="sm:right-10 right-4 rounded-md text-white bg-black bg-opacity-60 border-[1px] border-neutral-700 text-3xl z-20 p-2 sm:pl-8 pl-4 pr-4 flex items-center top-16 clip sm:mr-4">{buttonName} <span class="ml-2 sm:inline-block hidden"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short w-12 h-12 pt-1" viewBox="0 0 16 16">
+        <div class="flex h-full items-center w-full sm:w-auto justify-center sm:justify-normalz-20 pl-12 pr-12 blurBox">
+            <h2 class="sm:right-10 right-4 rounded-md text-white bg-black bg-opacity-60 border-[1px] border-neutral-700 text-3xl z-30 p-2 sm:pl-8 pl-4 pr-4 flex items-center top-16 clip">{buttonName} <span class="ml-2 sm:inline-block hidden"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short w-12 h-12 pt-1" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
             </svg></span></h2>
         </div>
@@ -10,7 +10,7 @@
         {/if}
     </div>
     {#if imgSrc}
-    <img class="absolute top-0 w-full h-full z-10 object-cover blur-sm opacity-100 duration-100 group-hover:opacity-70" alt="Train" src="{imgSrc}">
+    <img class="absolute top-0 w-full h-full z-10 object-cover blur-xs opacity-30 duration-100 group-hover:opacity-70" alt="Train" src="{imgSrc}">
     {/if}
     {#if !imgSrc}
         <div class="absolute top-0 w-full h-full z-10 bg-neutral-700 bg-opacity-60"></div>
@@ -30,3 +30,28 @@
         window.location.href = `/${clickRoute}`;
     }
 </script>
+
+<style>
+    .blurBox::after{
+        content: "";
+        position: absolute;
+        inset: 0;
+        z-index: 20;
+        width: 100%;
+
+        mask: linear-gradient(to right, transparent, black 60%);
+        backdrop-filter: blur(4px);
+    }
+
+    @media (min-width: 640px) {
+        .blurBox::after{
+            mask: none;
+        }
+    }
+
+    @media (max-width: 639px) {
+        .blurBox::after{
+            mask: linear-gradient(to right, transparent, black 0%);
+        }
+    }
+</style>
