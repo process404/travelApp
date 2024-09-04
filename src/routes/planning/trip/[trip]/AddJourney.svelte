170854dfc680e5d413d3ae09bde9f026990df676
process404 <88147999+process404@users.noreply.github.com>
2024-09-04 16:21:47 +0100

    layout

--- src/routes/planning/trip/[trip]/AddJourney.svelte
+++ src/routes/planning/trip/[trip]/AddJourney.svelte
@@ -1,11 +1,11 @@
-<button class="fixed w-full h-screen bg-black z-30 bg-opacity-10 backdrop-blur-xl flex items-center justify-center" on:click|self={close}>
-    <div class="bg-neutral-900 w-[90%] max-w-[800px] h-[90%] rounded-md p-4 z-40 cursor-default border-[1px] border-neutral-700">
+<button class="fixed w-full h-screen bg-black z-30 bg-opacity-10 backdrop-blur-xl flex items-center justify-center overflow-y-scroll" on:click|self={close}>
+    <div class="bg-neutral-900 w-[92%] max-w-[800px] h-[90%] rounded-md p-4 ml-3 z-40 cursor-default border-[1px] border-neutral-700">
         <div class="flex justify-between items-center">
-            <h2 class="text-white text-2xl font-semibold text-left">Add Journey <h4 class="text-neutral-500 italic text-xs inline-block ml-2">( Day {day} )</h4></h2>
+            <h2 class="text-white text-2xl font-semibold text-left">Add Journey <h4 class="text-neutral-500 italic text-xs inline-block sm:ml-2">( Day {day} )</h4></h2>
             <button class="fadeButton red p-1 pl-3 pr-3" on:click={close}>Cancel</button>
         </div>
-        <hr class="mt-2 mb-2 border-neutral-700">
-        <div class="flex gap-3 mt-4 border-[1px] border-neutral-700 rounded-md p-2 pb-3">
+        <hr class="mt-2 mb-4 border-neutral-700">
+        <div class="flex gap-2 mt-4 border-[1px] border-neutral-700 rounded-md p-2 pb-3 sm:flex-row flex-col">
             <div class="w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1">From</h3>
                 <PromptField ds="{locations}" on:select={selectFrom}/>
@@ -13,6 +13,26 @@
             <div class="w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1">To</h3>
                 <PromptField ds="{locations}" on:select={selectTo}/>
+            </div>
+        </div>
+        <div class="flex gap-3 mt-4 border-[1px] border-neutral-700 rounded-md p-2 pb-3 items-center md:flex-row flex-col">
+            <div class="w-full flex gap-3">
+                <div class="md:w-2/4 w-full">
+                    <h3 class="text-neutral-300 italic text-left mb-1">Departure Time</h3>
+                    <input class="standardInput iconEdit" type="time"/>
+                </div>
+                <div class="md:w-2/4 w-full">
+                    <h3 class="text-neutral-300 italic text-left mb-1">Arrival Time</h3>
+                    <input class="standardInput iconEdit" type="time"/>
+                </div>
+            </div>
+            <div class="md:w-1/2 w-full">
+                <h3 class="text-neutral-300 italic text-left mb-1">Service</h3>
+                <input class="standardInput"/>
+            </div>
+            <div class="w-full">
+                <h3 class="text-neutral-300 italic text-left mb-1">Operator</h3>
+                <input class="standardInput"/>
             </div>
         </div>
     </div>
