a35d0bc829def6a4e5b136773cce38d7fb73833b
process404 <88147999+process404@users.noreply.github.com>
2024-09-09 23:03:55 +0100

    add edit countries

--- src/routes/planning/trip/[trip]/AddJourney.svelte
+++ src/routes/planning/trip/[trip]/AddJourney.svelte
@@ -9,37 +9,37 @@
         <div class="flex gap-2 mt-4 border-[1px] border-neutral-700 rounded-md p-2 pb-3 sm:flex-row flex-col">
             <div class="w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1">From</h3>
-                <PromptField ds="{locations}" on:select={selectFrom} bind:value={from} ver="loc"/>
+                <PromptField ds="{locations}" on:select={selectFrom} bind:value={from} ver="loc" bind:presetC={fromCountry}/>
             </div>
             <div class="w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1">To</h3>
-                <PromptField ds="{locations}" on:select={selectTo} bind:value={to} ver="loc"/>
+                <PromptField ds="{locations}" on:select={selectTo} bind:value={to} ver="loc" bind:presetC={toCountry}/>
             </div>
         </div>
         <div class="flex gap-3 mt-4 border-[1px] border-neutral-700 rounded-md p-2 pb-3 items-center md:flex-row flex-col">
             <div class="w-full flex gap-3">
                 <div class="md:w-2/4 w-full">
                     <h3 class="text-neutral-300 italic text-left mb-1 text-sm">Departure Time</h3>
-                    <input class="input blue iconEdit" type="time" bind:value={departure}/>
+                    <input class="input blue iconEdit w-full" type="time" bind:value={departure}/>
                 </div>
                 <div class="md:w-2/4 w-full">
                     <h3 class="text-neutral-300 italic text-left mb-1 text-sm">Arrival Time</h3>
-                    <input class="input blue iconEdit" type="time" bind:value={arrival}/>
+                    <input class="input blue iconEdit w-full" type="time" bind:value={arrival}/>
                 </div>
             </div>
             <div class="md:w-1/2 w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1 text-sm">Service</h3>
-                <input class="input blue"  bind:value={service}/>
+                <input class="input blue w-full"  bind:value={service}/>
             </div>
             <div class="w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1 text-sm">Operator</h3>
-                <input class="input blue"  bind:value={operator}/>
+                <input class="input blue w-full"  bind:value={operator}/>
             </div>
         </div>
         <div class="flex gap-3 mt-4 border-[1px] border-neutral-700 rounded-md p-2 pb-3 items-center md:flex-row flex-col">
             <div class="w-full">
                 <h3 class="text-neutral-300 italic text-left mb-1 w-full">Description</h3>
-                <textarea class="input blue text-xs resize-none" maxlength="500" rows="4" placeholder="" bind:value={description}></textarea>
+                <textarea class="input blue text-xs resize-none w-full" maxlength="500" rows="4" placeholder="" bind:value={description}></textarea>
             </div>
         </div>
         <div class="flex gap-3 mt-auto border-[1px] border-neutral-700 rounded-md p-2 pb-2 items-center md:flex-row flex-col">
@@ -51,5 +51,101 @@
 </button>
 
 <script>
-import{createEventDispatcher}from"svelte";const dispatch=createEventDispatcher();export let day;import PromptField from"../../../../lib/components/PromptField.svelte";import CustomAlert from"../../../../lib/components/Alert.svelte";import{writable}from"svelte/store";let from,to,arrival,departure,service,operator,description,alrtMode=writable("err"),alrtTxt=writable(""),alrtAct=writable(!1);function close(o){dispatch("message",{text:"close"})}function submit(o){dispatch("message",{text:o})}let locations=[];try{const o=localStorage.getItem("locations");locations=JSON.parse(o)||[]}catch(o){}function selectFrom(o){from=o.detail.text,console.log(from,to)}function selectTo(o){to=o.detail.text,console.log(from,to)}function cLS(o){if(localStorage.getItem("locations")){locations.find((t=>t.toLowerCase()===o.toLowerCase()))?console.log("Location already exists"):(locations.push(o),localStorage.setItem("locations",JSON.stringify(locations)),console.log("Location added"))}else locations.push(o),localStorage.setItem("locations",JSON.stringify(locations)),console.log("Location added")}function genCode(){let o="",t="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";for(let e=0;e<6;e++)o+=t.charAt(Math.floor(72*Math.random()));return o}function addJourneyConfirm(){if(null==from||null==to||null==arrival||null==departure)return alrtTxt.set("Please fill in all fields"),alrtAct.set(!0),console.log("Please fill in all fields"),void console.log(from,to,arrival,departure);if(confirm("Please confirm you would like to enter this journey.")){let o={day:day,journey:{code:genCode(),from:from,to:to,arrival:arrival,departure:departure,service:service,operator:operator,description:description}};console.log("JI: ",o),submit(o),cLS(from),cLS(to)}}
+import { createEventDispatcher } from "svelte";
+const dispatch = createEventDispatcher();
+
+export let day;
+
+import PromptField from "../../../../lib/components/PromptField.svelte";
+import CustomAlert from "../../../../lib/components/Alert.svelte";
+import { writable } from "svelte/store";
+
+let from, to, arrival, departure, service, operator, description;
+let alrtMode = writable("err");
+let alrtTxt = writable("");
+let alrtAct = writable(false);
+
+let fromCountry = "GB"
+let toCountry = "GB"
+
+function close() {
+    dispatch("message", { text: "close" });
+}
+
+function submit(text) {
+    dispatch("message", { text });
+}
+
+let locations = [];
+try {
+    const storedLocations = localStorage.getItem("locations");
+    locations = JSON.parse(storedLocations) || [];
+} catch (error) {}
+
+function selectFrom(event) {
+    from = event.detail.text;
+    console.log(from, to);
+}
+
+function selectTo(event) {
+    to = event.detail.text;
+    console.log(from, to);
+}
+
+function addLocation(location) {
+    if (localStorage.getItem("locations")) {
+        if (locations.find((loc) => loc.toLowerCase() === location.toLowerCase())) {
+            console.log("Location already exists");
+        } else {
+            locations.push(location);
+            localStorage.setItem("locations", JSON.stringify(locations));
+            console.log("Location added");
+        }
+    } else {
+        locations.push(location);
+        localStorage.setItem("locations", JSON.stringify(locations));
+        console.log("Location added");
+    }
+}
+
+function generateCode() {
+    let code = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    for (let i = 0; i < 6; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
+
+function addJourneyConfirm() {
+    if (from === null || to === null || arrival === null || departure === null) {
+        alrtTxt.set("Please fill in all fields");
+        alrtAct.set(true);
+        console.log("Please fill in all fields");
+        console.log(from, to, arrival, departure);
+        return;
+    }
+
+    if (confirm("Please confirm you would like to enter this journey.")) {
+        const journey = {
+            day: day,
+            journey: {
+                code: generateCode(),
+                from: from,
+                fromCountry: fromCountry,
+                to: to,
+                toCountry: toCountry,
+                arrival: arrival,
+                departure: departure,
+                service: service,
+                operator: operator,
+                description: description,
+            },
+        };
+        console.log("JI: ", journey);
+        submit(journey);
+        addLocation(from);
+        addLocation(to);
+    }
+}
 </script>
