b64931edf54e3b463ae6a815fe2d293512fd1cd1
process404 <88147999+process404@users.noreply.github.com>
2024-09-10 22:43:23 +0100

    improvements

--- src/routes/planning/trip/[trip]/+page.svelte
+++ src/routes/planning/trip/[trip]/+page.svelte
@@ -12,7 +12,7 @@
     <Nav ver="back"/>
         <div class="flex flex-col items-center h-full justify-start overflow-y-scroll customScrollbar overflow-x-hidden">
             <div class="max-w-[1000px] w-full flex flex-col h-full">
-                <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-2 sm:mr-8 sm:pt-4 sm:pb-6 pl-4 pr-3 pb-4 overflow-y-scroll customScrollbar h-full">
+                <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-2 sm:mr-8 sm:pt-4 sm:pb-6 pl-4 pr-3 pb-4  customScrollbar h-auto">
                     {#if tripName == ''}
                         <h1 class="text-2xl text-white">Loading...</h1>
                         <span class="loader"></span>
