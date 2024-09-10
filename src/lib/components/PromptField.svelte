e4c78f6297e22191720d6b8a35f7201960f9e71d
process404 <88147999+process404@users.noreply.github.com>
2024-09-10 18:17:40 +0100

    less gap

--- src/lib/components/PromptField.svelte
+++ src/lib/components/PromptField.svelte
@@ -9,7 +9,7 @@
         <div class="absolute bottom-100 bg-neutral-800 border-[1px] border-neutral-700  p-2 w-full rounded-md rounded-t-none pl-4 pr-4 pb-4 z-50" style="filter:drop-shadow(0px 10px 20px rgba(0,0,0,0.5))">
             {#each locationSuggestions.slice(0,6) as name}
                 {#if name.name != value}
-                    <button on:click={selectLocationFrom(name)} class="text-white w-full text-sm text-left after:absolute after:bottom-[-0.3rem] after:hover:w-[97%] after:h-[1px] after:bg-white after:left-0 after:duration-100 after:w-0 before:absolute before:w-[97%] before:left-0 before:h-[1px] before:bg-neutral-600 before:top-[-0.33rem] first:before:hidden  mt-2 relative flex items-center gap-3">{name.name} <span><img src={`https://flagsapi.com/${name.country}/flat/64.png`} class="w-4 h-4" alt={name.country}></span></button>
+                    <button on:click={selectLocationFrom(name)} class="text-white w-full text-sm text-left after:absolute after:bottom-[-0.3rem] after:hover:w-[97%] after:h-[1px] after:bg-white after:left-0 after:duration-100 after:w-0 before:absolute before:w-[97%] before:left-0 before:h-[1px] before:bg-neutral-600 before:top-[-0.33rem] first:before:hidden  mt-2 relative flex items-center gap-2">{name.name} <span><img src={`https://flagsapi.com/${name.country}/flat/64.png`} class="w-4 h-4" alt={name.country}></span></button>
                 {/if}
             {/each}
         </div>
@@ -59,7 +59,7 @@
 
                 locationSuggestions.push(...filteredStations.map(set => ({ name: set.name, country: set.country })));
 
-                console.log(stns);
+                // console.log(stns);
 
                 const filteredStations2 = stns.filter(set => {
                     const lowerCaseName = set.name.toLowerCase();
