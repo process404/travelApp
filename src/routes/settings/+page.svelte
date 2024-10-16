958e156f728d3fe0255cb39e1ad708a8445d6531
process404 <88147999+process404@users.noreply.github.com>
2024-10-16 20:45:44 +0100

    fixed footer bug

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,7 +1,7 @@
-<div style="width: 100vw; display: flex; flex-direction: column" id="app">
+<div style="width: 100vw; height: 100vh; display: flex; flex-direction: column" id="app">
     <Nav ver="back"/>
-    <div class="h-full w-full flex flex-col items-center">
-        <div class="max-w-[1000px] flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-4 sm:mr-8 h-full sm:pt-6 sm:pb-6 pl-2 pr-2 sm:pl-12 sm:pr-12 pb-2">
+    <div class="h-auto w-full flex flex-col items-center">
+        <div class="max-w-[1000px] h-auto flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-4 sm:mr-8 sm:pt-6 sm:pb-6 pl-2 pr-2 sm:pl-12 sm:pr-12 pb-2">
             <h2 class="text-white text-2xl font-semibold sm:mt-1 mt-4 mb-4">Settings</h2>
             <div class="border-[1px] border-neutral-700 rounded-md w-full flex flex-col mt-4">
                 <div class="flex items-center gap-6 pt-4 pb-4 pl-3 pr-3">
@@ -43,14 +43,35 @@
                             {/if}
                         </div>
                     </div>
+                    <div class="flex items-center gap-6 pt-4 pb-4 pl-3 pr-3">
+                        <div class="w-3/4">
+                            <h2 class="dark:text-neutral-300 mb-2">Clear logs, journeys and photographs over 1, 3, 6 or 12 months old</h2>
+                            <p class="dark:text-neutral-400 text-sm italic">Clears all logs, journeys and associated photographs which are older than a certain time ago such as 1, 3, 6 or 12 months. <br><br><span class="text-yellow-300">Warning! This action is permenant and cannot be reversed.</span></p>
+                        </div>
+                        <div class="w-1/3 flex items-center justify-center" class:widthAdj={understood}>
+                            {#if understood2}
+                                <div class="w-full flex justify-end items-end flex-col gap-2">
+                                    <button class="button hover:before:bg-red-700 hover:before:bg-opacity-50 w-full" on:click={() => {clearTimePeriod("30")}}>Clear older than 1 month</button>
+                                    <button class="button hover:before:bg-red-700 hover:before:bg-opacity-50 w-full" on:click={() => {clearTimePeriod("60")}}>Clear older than 3 months</button>
+                                    <button class="button hover:before:bg-red-700 hover:before:bg-opacity-50 w-full" on:click={() => {clearTimePeriod("182")}}>Clear older than 6 months</button>
+                                    <button class="button hover:before:bg-red-700 hover:before:bg-opacity-50 w-full" on:click={() => {clearTimePeriod("365")}}>Clear older than 12 months</button>
+                                
+                                </div>
+                            {:else}
+                                <button class="button" on:click={() => {understood2 = true}}>Click to reveal options<br><span class="text-xs">(Acknowledge warning)</span></button>
+                            {/if}
+                        </div>
+                    </div>
                 </div>
                 {/if}
             </div>
         </div>
-    
-    
+        
+        
     </div>
-    <Footer/>
+    <div class="mt-auto">
+        <Footer/>
+    </div>
 </div>
 
 <script>
@@ -63,6 +84,7 @@
     let dbStn = false;
     let advancedDropdown = false;
     let understood = false;
+    let understood2 = false;
 
     onMount(() => {
         document.title = 'Settings';
@@ -131,6 +153,14 @@
 
     function clearPlanning(){
         if (confirm('Are you sure you want to clear all plans? This action cannot be undone.')) {
+            console.log('Journeys cleared');
+        } else {
+            console.log('Action cancelled');
+        }
+    }
+
+    function clearTimePeriod(timeInDays){
+        if (confirm(`Are you sure you want to clear all logs, journeys and associated photographs older than ${timeInDays} days? This action cannot be undone.`)) {
             console.log('Journeys cleared');
         } else {
             console.log('Action cancelled');
