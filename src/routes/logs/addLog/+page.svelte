2a4b8501b41ed567008092eda09cb4ccf272a62d
process404 <88147999+process404@users.noreply.github.com>
2024-10-17 01:35:16 +0100

    add log complete

--- src/routes/logs/addLog/+page.svelte
+++ src/routes/logs/addLog/+page.svelte
@@ -144,10 +144,10 @@
                                         <div class="flex gap-4 border-neutral-700 border-[1px] justify-between p-2 mt-2 first:mt-0">
                                             <img src={picture.src} alt={picture.alt} class="w-1/2 border-[1px] border-neutral-800 outline-none">
                                             <div class="w-full flex gap-1 flex-col">
-                                                <h3 class="text-neutral-300 italic">{picture.numbers.toString().replace(",",", ")}</h3>
+                                                <h3 class="text-neutral-300">{picture.numbers.toString().replace(",",", ")}</h3>
                                                 <div class="mt-auto flex items-end justify-end w-full gap-4 mr-1">
                                                     <!-- <h3 class="text-white text-xs sm:inline-block hidden">{picture.file_name}</h3> -->
-                                                    <button class="button hover:before:bg-red-700 hover:before:bg-opacity-50" on:click={() => {removeLogPhoto(picture)}}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <button class="button hover:before:bg-red-700 hover:before:bg-opacity-50" on:click={() => {removePicture(picture)}}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                       </svg>
                                                       </button>
@@ -259,7 +259,12 @@
                         {/if}
                         <div>
 
+                            
                         </div>
+                    </div>
+                    <div class="border-[1px] border-neutral-700 rounded-md sm:mt-8 mt-4 w-full max-w-[500px] p-4">
+                        <h3 class="text-neutral-300 italic">Notes</h3>
+                        <textarea class="input blue text-xs resize-none w-full mt-2" maxlength="500" rows="4" placeholder="" bind:value={inputNote}></textarea>
                     </div>
                     <div class="border-[1px] border-neutral-700 rounded-md sm:mt-16 mt-8 w-full max-w-[500px] p-4">
                     <button class="button blue w-full p-2 text-sm x-padding" on:click={confirmLog}>Submit Log</button>
@@ -309,6 +314,7 @@
     var alrtTxt  = writable('')
     var alrtAct = writable(false)
     var alrtMode = writable('err')
+    var inputNote = ''
 
     var db = import ('../../../db/vehicles.json');
     var logAreas = []
@@ -653,7 +659,16 @@
         });
     }
 
-    function removeLogPhoto(picture){
+    function removeLogPhoto(logItemId){
+        photoLogNumbers.update(numbers => {
+            return numbers.filter(number => number.id != logItemId);
+        });
+    }
+
+    function removePicture(picture){
+        if (confirm("Are you sure you want to remove this picture?")) {
+            pictures = pictures.filter(p => p.src != picture.src);
+        }
         pictures = pictures.filter(p => p.src != picture.src);
     }
 
@@ -720,7 +735,8 @@
                 log_time: inputTime,
                 log_lat: preciseLat,
                 log_lon: preciseLon,
-                pictures: pictures
+                pictures: pictures,
+                logNotes: inputNote,
             }));
 
             const addNew = JSON.parse(logs).concat(numbersWithLocation);
@@ -733,7 +749,8 @@
                 log_location: location,
                 log_date: inputDate,
                 log_time: inputTime,
-                pictures: pictures
+                pictures: pictures,
+                logNotes: inputNote,
             }));
 
             const addNew = JSON.parse(logs).concat(numbersWithLocation);
