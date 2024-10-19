ecc6c87bf28094aa4b40767d2920ace2e0143eb4
process404 <88147999+process404@users.noreply.github.com>
2024-10-19 19:55:34 +0100

    overflow fix

--- src/routes/overview/[date]/+page.svelte
+++ src/routes/overview/[date]/+page.svelte
@@ -1,7 +1,7 @@
 <div style="width: 100vw; display: flex; flex-direction: column" class="overflow-x-hidden" id="app">
     <Nav ver="back"/>
-    <div class="h-full w-full flex flex-col items-center pl-4 pr-4">
-        <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-4 sm:mr-8 h-screen sm:pt-6 sm:pb-6 pl-2 pr-2 pb-2 max-w-[1500px] w-full">
+    <div class="h-auto w-full flex flex-col items-center pl-4 pr-4">
+        <div class="flex flex-col items-center border-[1px] rounded-md border-neutral-700 sm:ml-8 ml-4 mr-4 sm:mr-8 min-h-screen h-auto sm:pt-6 sm:pb-6 pl-2 pr-2 pb-2 max-w-[1500px] w-full">
             {#if param != "unknown"}
                 <h2 class="text-white text-xl font-semibold sm:mt-1 mt-3">Overview for <span>{formatDate(param)}</span></h2>
             {:else}
