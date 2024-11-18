34703a62fbf6aaafa66e5227166c42f35b179280
process404 <88147999+process404@users.noreply.github.com>
2024-11-18 13:32:40 +0000

    add group removed

--- src/routes/overview/+page.svelte
+++ src/routes/overview/+page.svelte
@@ -24,13 +24,13 @@
                                 <option>{year}</option>
                             {/each}
                         </select>
-                        <button class="button blue2 text-sm" on:click={() => addGroupOpen()}>
+                        <!-- <button class="button blue2 text-sm" on:click={() => addGroupOpen()}>
                             Add Group
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                               </svg>
                               
-                        </button>
+                        </button> -->
                     </div>
                 </div>
                 {#if logsByYear.length == 0}
