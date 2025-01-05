0d5a2458232d039303ca32c27f3201e79530e8f4
process404 <88147999+process404@users.noreply.github.com>
2025-01-05 02:44:56 +0000

    undefined fix

--- src/routes/planning/trip/[trip]/+page.svelte
+++ src/routes/planning/trip/[trip]/+page.svelte
@@ -100,11 +100,11 @@
                                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
                                                                   </svg>
-                                                                  {#if journey.operator != '' && journey.service != ''}
+                                                                  {#if journey.operator != '' && journey.service != '' && journey.service != undefined}
                                                                         <h4 class="dark:text-white text-sm">{journey.service}<span class="ml-1 mr-1 opacity-30">/</span>{journey.operator}</h4>
                                                                   {:else if journey.operator != ''}
                                                                         <h4 class="dark:text-white text-sm">{journey.operator}</h4>
-                                                                  {:else if journey.service != ''}
+                                                                  {:else if journey.service != '' && journey.service != undefined}
                                                                         <h4 class="dark:text-white text-sm">{journey.service}</h4>
                                                                   {/if}
                                                                 </div>
